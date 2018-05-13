70514d42a1a1638a07f307ff2240994a9726a170
David Vo <auscompgeek@users.noreply.github.com>
2018-05-13 13:10:15 +1000

    Expose ParamEnum at top level

--- src/ctre/mod.rs
+++ src/ctre/mod.rs
@@ -4,5 +4,5 @@ pub mod bindings;
 pub mod motor_control;
 pub mod motion;
 
-pub use ctre::bindings::ErrorCode;
+pub use ctre::bindings::{ErrorCode, ParamEnum};
 pub type Result<T> = ::std::result::Result<T, ErrorCode>;
