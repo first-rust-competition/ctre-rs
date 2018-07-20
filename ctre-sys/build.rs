e8d15c143e91f459897f096a42076896b7f8281d
David Vo <auscompgeek@users.noreply.github.com>
2018-07-21 00:38:06 +1000

    Fix build.rs to link to NI libraries

--- ctre-sys/build.rs
+++ ctre-sys/build.rs
@@ -1,6 +1,20 @@
 // use std::env;
-// use std::path::PathBuf;
+
+const LIB_LIST: &'static [&'static str] = &[
+    "FRC_NetworkCommunication",
+    "NiFpga",
+    "NiFpgaLv",
+    "niriodevenum",
+    "niriosession",
+    "NiRioSrv",
+    "RoboRIO_FRC_ChipObject",
+    "visa",
+];
 
 fn main() {
-    println!("cargo:rustc-link-lib=CTRE_PhoenixCCI");
+    println!("cargo:rustc-link-lib=static=CTRE_PhoenixCCI");
+    for lib in LIB_LIST.iter() {
+        println!("cargo:rustc-link-lib=dylib={}", lib);
+    }
+    println!("cargo:rustc-link-lib=stdc++");
 }
