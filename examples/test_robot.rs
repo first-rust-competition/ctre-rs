280623d87db4b8c4836da95c4f7bfa80d33ab25d
David Vo <auscompgeek@users.noreply.github.com>
2018-09-10 14:12:54 +1000

    Add limit switch config to example

--- examples/test_robot.rs
+++ examples/test_robot.rs
@@ -3,13 +3,18 @@ use ctre::motor_control::*;
 use std::{thread, time};
 
 /// Don't actually do this.  Ideally you would use a WPILib port with a nice Robot abstraction.
-fn main() {
+fn main() -> ctre::Result<()> {
     let talon = TalonSRX::new(0);
-    let ten_ms = time::Duration::from_millis(10);
+    let delay = time::Duration::from_millis(20);
+    talon.config_forward_limit_switch_source(
+        LimitSwitchSource::FeedbackConnector,
+        LimitSwitchNormal::NormallyOpen,
+        10,
+    ).into_res()?;
     loop {
         talon.set(ControlMode::PercentOutput, 0.5, DemandType::Neutral, 0.0);
-        thread::sleep(ten_ms);
+        thread::sleep(delay);
         talon.neutral_output();
-        thread::sleep(ten_ms);
+        thread::sleep(delay);
     }
 }
