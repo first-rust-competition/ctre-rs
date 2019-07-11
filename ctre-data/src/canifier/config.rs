c0f9a78597df543ceaf4bcb1843b24b32e251ffb
David Vo <auscompgeek@users.noreply.github.com>
2019-07-11 22:17:49 +1000

    ctre-data: canifier/config: Fix defaults

--- ctre-data/src/canifier/config.rs
+++ ctre-data/src/canifier/config.rs
@@ -3,13 +3,18 @@
 use super::super::CustomParamConfiguration;
 use super::cci::CANifierVelocityMeasPeriod;
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[derive(Debug, SmartDefault, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(default))]
 pub struct CANifierConfiguration {
     pub custom_param: CustomParamConfiguration,
     pub velocity_measurement_period: CANifierVelocityMeasPeriod,
+    #[default = "64"]
     pub velocity_measurement_window: i32,
+    /// Whether to clear sensor position on forward limit
     pub clear_position_on_limit_f: bool,
+    /// Whether to clear sensor position on reverse limit
     pub clear_position_on_limit_r: bool,
+    /// Whether to clear sensor position on index
     pub clear_position_on_quad_idx: bool,
 }
